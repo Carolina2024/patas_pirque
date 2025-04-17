524de04c16219ddefa478dfac570cf3ef511cb12
Angel Añez <angdavid2910@gmail.com>
2025-04-17 18:52:31 -0400

    refactor: translate error messages to spanish

--- backend/src/modules/Auth/dto/loginUser.dto.ts
+++ backend/src/modules/Auth/dto/loginUser.dto.ts
@@ -1,8 +1,11 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginUserDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'Ingrese un correo con un formato válido' })
+  @IsNotEmpty({ message: 'El correo es requerido' })
   email!: string;
-  @IsString()
+
+  @IsString({message: "La contraseña debe ser una cadena de caracteres"})
+  @IsNotEmpty({message: 'La contraseña es requerida'})
   password!: string;
 }
