f88284a9da069d4437322c9ea645db84b4b18d3b
Angel Añez <angdavid2910@gmail.com>
2025-04-17 11:10:55 -0400

    fix: add missing modules at app and auth module

--- backend/src/modules/Auth/auth.module.ts
+++ backend/src/modules/Auth/auth.module.ts
@@ -3,9 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../User/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: "60s" },
+  }),],
   controllers: [AuthController],
   providers: [AuthService],
 })
