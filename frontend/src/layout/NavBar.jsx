48c83942346bc0906056ec04e12c1192cd58f2cc
Carolina2024 <carcastillos@gmail.com>
2025-04-22 20:18:41 -0400

    Update login form

--- frontend/src/layout/NavBar.jsx
+++ frontend/src/layout/NavBar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             <>
               <Link to="/login">
                 <button className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-secundary hover:text-tertiary cursor-pointer">
-                  Login
+                  Ingresar
                 </button>
               </Link>
               <Link to="/register">
